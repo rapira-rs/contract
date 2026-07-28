c197b0f6a6f85aec276248bba360287b90787b9a
roxblnfk <roxblnfk@ya.ru>
2026-07-29 00:01:42 +0400

    Add LogLevel enum to define PSR-3 log severity levels

---
+++ src/Plugin/Logger/LogLevel.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rapira\Plugin\Logger;
+
+/**
+ * Severity of a log message. Values match PSR-3 log levels, so a PSR wrapper
+ * maps onto them one to one.
+ */
+enum LogLevel: string
+{
+    case Emergency = 'emergency';
+    case Alert = 'alert';
+    case Critical = 'critical';
+    case Error = 'error';
+    case Warning = 'warning';
+    case Notice = 'notice';
+    case Info = 'info';
+    case Debug = 'debug';
+}
